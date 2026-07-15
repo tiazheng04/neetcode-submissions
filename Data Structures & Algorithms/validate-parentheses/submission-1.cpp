503ab64dfc9b1a5279bfc196052d24725d0fc9d4
Tia <tia.zheng20@gmail.com>
2026-07-14 22:36:10 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i]== '(' || s[i]== '{' || s[i] =='['){
+                st.push(s[i]);
+            } else{
+                if(st.empty()){
+                    return false;
+                }
+                char cur = st.top();
+                if((cur == '(' && s[i] == ')') || (cur == '{' && s[i] == '}') || (cur == '[' && s[i] == ']')){
+                    st.pop();
+                } else{
+                    return false;
+                }
+            }
+        }
+        if(st.empty()){
+            return true;
+        } else{
+            return false;
+        }
+    }
+};
